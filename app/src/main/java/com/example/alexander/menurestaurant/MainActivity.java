5dc34ba3ccf77371c5d64bc6c4af65eeae789810
Skender <alexasp3@gmail.com>
2018-05-11 22:13:49 +0300

    Реализована основная верстка приложения

--- app/src/main/java/com/example/alexander/menurestaurant/MainActivity.java
+++ app/src/main/java/com/example/alexander/menurestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexander.menurestaurant;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
         setContentView(R.layout.activity_main);
     }
 }
